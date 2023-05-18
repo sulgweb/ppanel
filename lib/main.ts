6662544fb597367e832fd3a9a0fd8acebd5cbc0c
xiaoyu <xiaoyu@sulg.top>
2023-05-18 23:04:17 +0800

    添加web-vitals参数，添加固定位置逻辑

--- lib/main.ts
+++ lib/main.ts
@@ -1,7 +1,12 @@
 import { onCLS, onFCP, onFID, onINP, onLCP, onTTFB } from "web-vitals";
 export function startPPanel() {
+  // 更新web-vitals数据
   const paintHandle = (e) => {
-    console.log("paintHandle", e);
+    const pre = "_ppanel_";
+    const dom = document.getElementById(`${pre}${e.name}`);
+    const text = `${e.name}:${(e.value / 1000).toFixed(2)}s`;
+    console.log(dom, text);
+    dom.innerText = `${e.name}：${(e.value / 1000).toFixed(2)}s`;
   };
   onCLS(paintHandle);
   onFCP(paintHandle);
@@ -18,27 +23,62 @@ export function startPPanel() {
   container.id = id;
   container.setAttribute(
     "style",
-    "width: 250px; height: 420px; padding: 16px;background-color: #001529; position: fixed; left: 0; top: 0;cursor: pointer;box-shadow: 0px 0px 5px rgba(0,0,0,0.3);border-radius: 8px;"
+    "width: 160px; height: 240px; padding: 12px;box-sizing: content-box;background-color: #001529; position: fixed; left: 0; top: 0;cursor: pointer;box-shadow: 0px 0px 5px rgba(0,0,0,0.3);border-radius: 8px;z-index:999999"
   );
-  container.innerHTML = `<canvas id="_fps_canvas" width="250" height="200" style="background-color: #001c36;margin-bottom:10px;border-radius: 8px;"></canvas>
-    <canvas id="_memory_canvas" width="250" height="200" style="background-color: #001c36;border-radius: 8px;"></canvas>`;
+  container.innerHTML = `<canvas id="_fps_canvas" width="160" height="80" style="background-color: #001c36;margin-bottom:10px;border-radius: 8px;"></canvas>
+    <canvas id="_memory_canvas" width="160" height="80" style="background-color: #001c36;border-radius: 8px;"></canvas>
+    <div id='_web_vitals' style="display:flex;flex-wrap: wrap;font-size:12px;color:#ccc;margin-top:4px;">
+      <div id='_ppanel_CLS' style='width:50%;'>CLS：无</div>
+      <div id='_ppanel_FCP' style='width:50%;'>FCP：无</div>
+      <div id='_ppanel_FID' style='width:50%;'>FID：无</div>
+      <div id='_ppanel_INP' style='width:50%;'>INP：无</div>
+      <div id='_ppanel_LCP' style='width:50%;'>LCP：无</div>
+      <div id='_ppanel_TTFB' style='width:50%;'>TTFB：无</div>
+    </div>
+    `;
   document.body.appendChild(container);
 
   let panel = document.getElementById("_performance_panel") as HTMLDivElement;
-  var disX = 0;
-  var disY = 0;
-  panel.onmousedown = function (ev) {
-    disX = ev.clientX - panel.offsetLeft;
-    disY = ev.clientY - panel.offsetTop;
-    document.onmousemove = function (ev) {
-      panel.style.left = ev.clientX - disX + "px";
-      panel.style.top = ev.clientY - disY + "px";
-    };
-    document.onmouseup = function () {
-      document.onmousemove = null;
-      document.onmouseup = null;
-    };
+  let canMovePanel = false;
+  let disX = 0;
+  let disY = 0;
+
+  const initPanel = () => {
+    panel.style.left = localStorage.getItem("_ppanel_left") || "0px";
+    panel.style.top = localStorage.getItem("_ppanel_top") || "0px";
   };
+  initPanel();
+
+  const onMouseDown = (ev) => {
+    canMovePanel = true;
+    const cur = ev.touches ? ev.touches[0] : ev;
+    disX = cur.clientX - panel.offsetLeft;
+    disY = cur.clientY - panel.offsetTop;
+  };
+
+  const onMouseMove = (ev) => {
+    if (canMovePanel) {
+      const cur = ev.touches ? ev.touches[0] : ev;
+      panel.style.left = cur.clientX - disX + "px";
+      panel.style.top = cur.clientY - disY + "px";
+    }
+  };
+
+  const onMouseUp = () => {
+    canMovePanel = false;
+    localStorage.setItem("_ppanel_left", panel.style.left);
+    localStorage.setItem("_ppanel_top", panel.style.top);
+  };
+
+  // 鼠标事件
+  panel.addEventListener("mousedown", onMouseDown);
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
+
+  // 触摸事件
+  panel.addEventListener("touchstart", onMouseDown);
+  document.addEventListener("touchmove", onMouseMove);
+  document.addEventListener("touchend", onMouseUp);
 
   let fpsCanvas = document.getElementById("_fps_canvas") as HTMLCanvasElement;
   let fpsCtx = fpsCanvas && fpsCanvas.getContext("2d");
@@ -48,7 +88,7 @@ export function startPPanel() {
   let memoryCtx = memoryCanvas && memoryCanvas.getContext("2d");
   let fpsObj = {
     unit: "FPS",
-    num: 50,
+    num: 40,
     max: 0,
     min: null,
     list: [],
@@ -58,7 +98,7 @@ export function startPPanel() {
   };
   let memoryObj = {
     unit: "MB",
-    num: 50,
+    num: 40,
     max: 0, // performance.memory.jsHeapSizeLimit / 1048576,
     min: null,
     list: [],
@@ -123,17 +163,17 @@ export function startPPanel() {
         obj.min
       }-${obj.max}) `;
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.font = "bold 20px serif";
+      ctx.font = "bold 14px serif";
       ctx.fillStyle = `rgba(${color},1)`;
       const textWidth = ctx.measureText(text).width;
-      ctx.fillText(text, canvas.width - textWidth + 5, 24);
+      ctx.fillText(text, canvas.width - textWidth + 5, 16);
 
       // 绘制线
       ctx.moveTo(0, obj.list[0]);
       ctx.beginPath();
-      const _max = obj.max + 50;
+      const _max = obj.max + 24;
       for (let i = 0; i < obj.list.length; i++) {
-        let x = i * 5;
+        let x = i * 4;
         let y = _max - obj.list[i];
         ctx.lineTo(x, y);
       }
@@ -148,12 +188,12 @@ export function startPPanel() {
       ctx.moveTo(0, obj.list[0]);
       ctx.beginPath();
       for (let i = 0; i < obj.list.length; i++) {
-        let x = i * 5;
+        let x = i * 4;
         let y = _max - obj.list[i];
         ctx.lineTo(x, y);
         if (i === obj.list.length - 1) {
-          ctx.lineTo(x, 200);
-          ctx.lineTo(0, 200);
+          ctx.lineTo(x, 80);
+          ctx.lineTo(0, 80);
           ctx.lineTo(0, _max - obj.list[0]);
         }
       }
